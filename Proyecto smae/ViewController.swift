5b43a1f0773e76383de6751e3393cefd13279e6a
Luis Humberto <beto020017@gmail.com>
2021-11-10 14:02:28 -0600

    Initial Commit

---
+++ Proyecto smae/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Proyecto smae
+//
+//  Created by Luis Humberto Martinez Echegaray on 10/11/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
