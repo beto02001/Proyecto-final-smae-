18d5b2062f19c15a5dcbd9f9dbfc1cb05987de10
Luis Humberto <beto020017@gmail.com>
2021-11-11 02:05:53 -0600

    archivos .swift

--- Proyecto smae/ViewController.swift
+++ Proyecto smae/ViewController.swift
@@ -7,13 +7,40 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+
+    @IBOutlet weak var tablaGrupoAlimentos: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        tablaGrupoAlimentos.delegate = self
+        tablaGrupoAlimentos.dataSource = self
     }
 
 
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return alimentos.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt renglon: IndexPath) -> UITableViewCell {
+        let celda = tablaGrupoAlimentos.dequeueReusableCell(withIdentifier: "celdaAlimento", for: renglon) as! VistaCelda
+        let lista = alimentos[renglon.row]
+        celda.imagenCeldaGrupo.image = UIImage(named: lista.grupo)
+        celda.textoCeldaGrupo.text = lista.nombre
+        return celda
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.destination is ViewAlimentos{
+            let renglon = tablaGrupoAlimentos.indexPathForSelectedRow!.row
+            let listaAlimento = alimentos[renglon].elementos
+            let informacion = segue.destination as? ViewAlimentos
+            informacion?.listaDeAlimentos = listaAlimento
+            informacion?.nombreGrupo = alimentos[renglon].grupo
+        }
+    }
+    
+    
 }
 
---
+++ Proyecto smae/VistaCelda.swift
@@ -0,0 +1,26 @@
+//
+//  VistaCelda.swift
+//  Proyecto smae
+//
+//  Created by Luis Humberto Martinez Echegaray on 10/11/21.
+//
+
+import UIKit
+
+class VistaCelda: UITableViewCell {
+
+    @IBOutlet weak var imagenCeldaGrupo: UIImageView!
+    @IBOutlet weak var textoCeldaGrupo: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
